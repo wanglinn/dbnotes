41641ebfd0dcc744c1def21723fd0f900a70340a
wln <wanglin3@asiainfo.com>
2019-01-27 23:38:01 +0800

    Update limit.c

--- postgres/path/limit.c
+++ postgres/path/limit.c
@@ -15,9 +15,30 @@ if (!qry->limitCount)
 		qry->limitCount = transformLimitClause(pstate, (Node *)n, EXPR_KIND_LIMIT, "LIMIT");
 } 
 
+---
+测试：
+postgres=# explain verbose select * from t1;
+                            QUERY PLAN                             
+-------------------------------------------------------------------
+ Limit  (cost=0.00..0.14 rows=10 width=4)
+   Output: id
+   ->  Seq Scan on public.t1  (cost=0.00..35.50 rows=2550 width=4)
+         Output: id
+(4 rows)
+		 
 这样修改带来的问题就是：
 create table t1(id int);
 insert into t1 select generate_series(1,100); -- 实际只插入10行
 
+postgres=# explain verbose insert into t1 select generate_series(1,100); 
+                         QUERY PLAN                          
+-------------------------------------------------------------
+ Insert on public.t1  (cost=0.00..0.15 rows=10 width=4)
+   ->  Limit  (cost=0.00..0.05 rows=10 width=4)
+         Output: (generate_series(1, 100))
+         ->  ProjectSet  (cost=0.00..5.02 rows=1000 width=4)
+               Output: generate_series(1, 100)
+               ->  Result  (cost=0.00..0.01 rows=1 width=0)
+(6 rows)
 
         
